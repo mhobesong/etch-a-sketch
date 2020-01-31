f8e52b857ab1dafe542168f26b1d34941ce823f8
Moses Besong Besong <mosbesong@gmail.com>
2020-01-31 04:26:41 +0100

    add background to drawing area

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,13 @@ function createDIVs(number_of_divs){
 	for (let i=0; i < number_of_divs; i++){
 		let element = document.createElement("div");
 		element.setAttribute("class", "grid-item");
-		element.style.backgroundColor = "white";
-		element.style.opacity = 0;
+
+		let red = Math.floor(Math.random() * 257);
+		let green = Math.floor(Math.random() * 257);
+		let blue = Math.floor(Math.random() * 257);
+
+		element.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+		element.style.opacity = 0.1;
 
 		element.addEventListener("mouseover", function(e){
 			e.target.style.backgroundColor = "black";
