e684553018b37667ba3f02a823ff075a3619f348
Moses Besong Besong <mosbesong@gmail.com>
2020-01-31 04:16:18 +0100

    add opacity to sketch

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,20 @@ function createDIVs(number_of_divs){
 	for (let i=0; i < number_of_divs; i++){
 		let element = document.createElement("div");
 		element.setAttribute("class", "grid-item");
-		element.style.backgroundColor = "rgba(" + Math.floor(Math.random() * 257) + "," + Math.floor(Math.random() * 257) + "," + Math.floor(Math.random() * 257) + ", 0.1)";
+		element.style.backgroundColor = "white";
+		element.style.opacity = 0;
+
+		element.addEventListener("mouseover", function(e){
+			e.target.style.backgroundColor = "black";
+			e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.4;
+		});
+
 		document.querySelector("#container").appendChild(element);
 	}
 }
 
 document.querySelector("button").addEventListener("click", function(){
-	let number_of_divs = parseInt(prompt("Enter the number of divs to display"));
+	let number_of_divs = parseInt(prompt("Enter the number of divs to display", 300));
 	document.querySelector("#container").innerHTML = "";
 	createDIVs(number_of_divs);
 });
